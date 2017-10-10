7e42ed252e3670221fa661003de6d8428e70dc95
20076356 <20076356@IT101PC17.wit.ie>
2017-10-10 14:33:58 +0100

    added hashCode method to user

--- src/models/User.java
+++ src/models/User.java
@@ -1,5 +1,6 @@
 package models;
 import static com.google.common.base.MoreObjects.toStringHelper;
+import com.google.common.base.Objects;
 
 public class User 
 {
@@ -20,6 +21,7 @@ public class User
     this.password = password;
   }
   
+  @Override
   public String toString()
   {
 	  return toStringHelper(this).addValue(firstName)
@@ -27,6 +29,12 @@ public class User
               .addValue(password)
               .addValue(email)                               
               .toString();
+  }
+  
+  @Override
+  public int hashCode()
+  {
+	  return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);
   }
   
   
